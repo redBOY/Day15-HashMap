4afc9bc31e26085a2dcee0827c2d5cc6c4a74ce0
“Adithya <adithyasrinivas66@gmail.com>
2022-09-03 17:03:48 +0530

    initial commit

---
+++ MyMapNode.java
@@ -0,0 +1,13 @@
+package bridgelabzHashMap;
+
+public class MyMapNode<K,V> {
+
+    K key;
+    V value;
+    MyMapNode<K,V> next;
+
+    public MyMapNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
